16ecd7f63fcdcee98c3527f4c1044a8b396b5d47
Kaijun <kaijun_huang@outlook.com>
2023-05-29 16:35:30 +0800

    feat: write deploy scripts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.scss';
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from 'context/AuthContext';
+
+const basename = process.env.PUBLIC_URL;
+
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <AuthProvider>
           <Routes>
             <Route path="todos" element={<TodoPage />} />
